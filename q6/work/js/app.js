f1888c3c53338bfde9d061b899d0fe22e59574aa
inagemika <mikainage7@gmail.com>
2024-02-04 06:18:40 +0900

    jquery_practice

--- q6/work/js/app.js
+++ q6/work/js/app.js
@@ -1,28 +1,32 @@
 $(function() {
-    $(".select-box").on("change", function() {
-//セレクトボックス内のどれか一つの項目(選択肢）をクリックするとイベントを実行
-    const allFood =$(this).val(),
-        foodElement=$(".food-list li");
-//セレクトボックス内の項目(選択肢）の中のすべての値を取得
-    if("all" === allFood){
-        foodElement.show();
-//「全て」の項目は、各項目の中の全ての値と等しいとき、「全て」がクリックされれば、すべての値が表示されるが、
-    }else{
-//「全て」の項目以外の別の項目がクリックされた場合は、
-        $.each(foodElement, function(index,item) {
-//各食べ物(値)に繰り返し処理し、クリックされた項目と共通する値だけを抽出して表示
-            const foodCategory = $(item).data("category-type");
-//各食べ物(値)をすべて取得
-            if(allFood === foodCategory){
-//「全て」の項目は、各項目の中の全ての値と各食べ物(値)とも等しいので、
+        $(".select-box").on("change", function() {
+        //セレクトボックス内のどれか一つの項目(選択肢）をクリックするとイベントを実行
+        const allFood =$(this).val(),
+                foodName=$(".food-list li");
+        //セレクトボックス内の項目(選択肢）の中のすべての値を取得
+        if("all" === allFood){
+                foodName.show();
+        //「全て」の項目は、各項目の中の全ての値と等しいとき、「全て」がクリックされれば、すべての値が表示されるが、
+        }else{
+        //「全て」の項目以外の別の項目がクリックされた場合は、
+                $.each(foodName, function(index,item) {
+                //各食べ物(値)に繰り返し処理し、クリックされた項目と共通する値だけを抽出して表示
+                const foodCategory = $(item).data("category-type");
+                //各食べ物(値)をすべて取得
+                const indexNum = (index + 0) + " : " + allFood;
+                $(item).data(indexNum)
+                //index番号を0番から順に各食べ物に指定
+                console.log(indexNum);
+                if(allFood === foodCategory){
+                //「全て」の項目は、各項目の中の全ての値と各食べ物(値)とも等しいので、
                 $(item).show()
-//「全て」がクリックされれば、すべての値が表示されるが、
-            }else{
-//「全て」の項目以外の別の項目がクリックされた場合は
+                //「全て」がクリックされれば、すべての値が表示されるが、
+                }else{
+                //「全て」の項目以外の別の項目がクリックされた場合は
                 $(item).hide()
-//すべて表示されていた値は非表示になる
-            }
-        })
-    }
-    });
+                //すべて表示されていた値は非表示になる
+                }
+                })
+        }
+        });
 });
--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -13,15 +13,21 @@
 $(function() {
     let pageCount = 1,
     //初期値は、１に設定
-        searchword = "";
+        searchNoword = "";
         //入力する内容は何も入っていない
     $(".search-btn").on("click", function() {
     //検索ボタンをクリックしたとき
         let searchWord = $("#search-input").val();
         //検索フォームで入力されるすべての値を表示
-        searchWord !== searchword ? (pageCount = 1,$(".lists").empty(),searchword = searchWord) : pageCount++;
+        if(searchWord !== searchNoword) {
         //検索されるワードが入力されているときと、検索ワードが入力されていないときは異なる条件が正しいとき、
+            pageCount = 1,
+            $(".lists").empty(),
+            searchNoword = searchWord
         //初期値１になる,検索結果で表示される詳細は何もなし(違う検索ワードで検索するため),検索ワードが入力されていない状態になる
+        }else{
+            pageCount++;
+        }
         //検索されるワードが入力されているときと、検索ワードが入力されていないときは異なる条件が異なるとき、そのまま同じ検索ワードで検索する
     $.ajax({
         url: "https://ci.nii.ac.jp/books/opensearch/search?title="+ searchWord + "&format=json&p=" + pageCount + "&count=20",
@@ -32,25 +38,25 @@ $(function() {
     //通信が成功したとき
         searcResult(response["@graph"])
         //検索結果として、@graphの詳細データを表示
-    }).fail(function(response) {
+    }).fail(function(xhr,status,error) {
     //通信が失敗したとき
     $(".lists").empty();
     //検索結果で表示される詳細は何もなし
     $(".message").remove();
     //メッセージとして表示される要素を削除
-    if(0 === response.status) {
+    if(xhr.status === 0) {
     //検索結果として表示される各項目の詳細が0のとき、
-        $(".lists").before('<div class="message">正常に通信できませんでした。<br>インターネットの接続の確認をしてください。</div>')
+        $(".lists").before(status = '<div class="message">正常に通信できませんでした。<br>インターネットの接続の確認をしてください。</div>')
         //リスト要素の直前に「正常に通信できませんでした。、インターネットの接続の確認をしてください。」を表示
     }else{
     //検索結果として表示される各項目の詳細が0と異なり、
-        if(400 === response.status) {
+        if(xhr.status === 400) {
         //検索結果として表示される各項目の詳細が400でエラーになったとき、
-        $(".lists").before('<div class="message">検索キーワードが有効ではありません。<br>1文字以上で検索して下さい。</div>')
+        $(".lists").before(status = '<div class="message">検索キーワードが有効ではありません。<br>1文字以上で検索して下さい。</div>')
         //リスト要素の直前に「検索キーワードが有効ではありません。、1文字以上で検索して下さい。」を表示
-    }else{
-    //検索結果として表示される各項目の詳細が400のエラーと異なるとき、
-        $(".lists").before('<div class="message">予期せぬエラーが起きました。<br>再読み込みを行ってください。</div>')
+        }else{
+        //検索結果として表示される各項目の詳細が400のエラーと異なる例外のとき、
+        $(".lists").before(error = '<div class="message">予期せぬエラーが起きました。<br>再読み込みを行ってください。</div>')
         //リスト要素の直前に「予期せぬエラーが起きました。、再読み込みを行ってください。」を表示
     }
     }
@@ -60,34 +66,42 @@ $(function() {
     //検索ワードとして入力された値に対応する検索結果を表示
         $(".message").remove();
         //メッセージとして表示される要素を削除
-        let response;
-        //検索結果として表示される詳細データの初期値を設定しない
-        if(0 < (null == (response = searchWord[0].items) ? void 0 : response.length)){
-        /*0より大きい値(検索結果が見つかり表示される状態）の時、もし取得された各項目の0番目の最初の詳細(items)が何もない（null)場合、undefinedとなり何も詳細がない未定の状態となり(void 0)、
-        取得された各項目の0番目の最初の詳細(items)があった場合、取得される詳細の個数を表示（検索結果として見つかった0番目の最初の詳細から順に表示（response.length)）*/
-            $.each(searchWord[0].items, function(index,data) {
-            //検索ワードとして入力された値に対応する検索結果として表示される値の、0番目の値からループ処理で各項目と共通する値を抽出して表示
-                let resultTxt = '<li class="lists-item"><div class="list-inner"><p>タイトル：' +
-            //リスト要素(各項目の要素)を追加、「タイトル：」の項目を追加
-                ((data.title ? data.title : "タイトル不明") +
-                //レスポンス内容の"@graph"の中の「タイトル」の詳細を取得できたら表示、取得できない場合は、「タイトル不明」を表示
-                "</p><p>著者名：" ) +
-                //リスト要素に、「著者名：」の項目を追加
-                ((data["dc:creator"] ? data["dc:creator"] : "著者名不明") +
-                //レスポンス内容の"@graph"の中の「著者名」の詳細を取得できたら表示、取得できない場合は、「著者名不明」を表示
-                "</p><p>出版社：" ) +
-                //リスト要素に、「出版社：」の項目を追加
-                ((data["dc:publisher"] ? data["dc:publisher"][0] : "出版社不明") +
-                //レスポンス内容の"@graph"の中の「出版社」の詳細を取得できたら表示、取得できない場合は、「出版社不明」を表示
-                '</p><a href="' ) +
-                (data.link["@id"] +
-                '"target="_blank">書籍情報</a></div></li>');
-                //リスト要素に、「書籍情報」の項目を追加し、レスポンス内容の"@graph"の中の書籍情報のURLを設定、詳細を表示する欄は何も表示せず別タブで表示
-                $(".lists").prepend(resultTxt)
-                //各項目のタイトルは、取得された各詳細の要素の先頭に表示する
-            })
+        let messageLength = 0
+        //メッセージ部分の値が0で何もなし
+        if (null == searchWord[0].items) {
+        //もし取得された各項目の0番目の最初の詳細が何もない場合
+            messageLength = ''
+        //メッセージ部分の値が何もなく表示されない
         }else{
-        //上記の条件が、0より大きい値ではない場合、
+        //取得された各項目の0番目の最初の詳細がある場合
+            messageLength = searchWord[0].items.length
+        //メッセージ部分の値は、検索結果として見つかった0番目の最初の詳細から順に表示
+        }
+        if(0 < messageLength){
+        //メッセージ部分に表示される、検索結果の詳細を取得でき表示される場合
+        $.each(searchWord[0].items, function(index,data) {
+            //検索ワードとして入力された値に対応する検索結果として表示される値の、0番目の値からループ処理で各項目と共通する値を抽出して表示
+            const resultHtml = '<li class="lists-item"><div class="list-inner"><p>タイトル：' +
+            //リスト要素(各項目の要素)を追加、「タイトル：」の項目を追加
+            ((data.title ? data.title : "タイトル不明") +
+            //レスポンス内容の"@graph"の中の「タイトル」の詳細を取得できたら表示、取得できない場合は、「タイトル不明」を表示
+            "</p><p>著者名：" ) +
+            //リスト要素に、「著者名：」の項目を追加
+            ((data["dc:creator"] ? data["dc:creator"] : "著者名不明") +
+            //レスポンス内容の"@graph"の中の「著者名」の詳細を取得できたら表示、取得できない場合は、「著者名不明」を表示
+            "</p><p>出版社：" ) +
+            //リスト要素に、「出版社：」の項目を追加
+            ((data["dc:publisher"] ? data["dc:publisher"][0] : "出版社不明") +
+            //レスポンス内容の"@graph"の中の「出版社」の詳細を取得できたら表示、取得できない場合は、「出版社不明」を表示
+            '</p><a href="' ) +
+            (data.link["@id"] +
+            '"target="_blank">書籍情報</a></div></li>');
+            //リスト要素に、「書籍情報」の項目を追加し、レスポンス内容の"@graph"の中の書籍情報のURLを設定、詳細を表示する欄は何も表示せず別タブで表示
+            $(".lists").prepend(resultHtml)
+            //各項目のタイトルは、取得された各詳細の要素の先頭に表示する
+        })
+        }else{
+        //メッセージ部分に表示される、検索結果が不明で詳細を取得できない場合
             $(".lists").before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索して下さい。</div>')
             //リスト要素の直前に「検索結果が見つかりませんでした。、別のキーワードで検索して下さい。」を表示
         }
@@ -95,7 +109,7 @@ $(function() {
     $(".reset-btn").on("click", function() {
     //リセットボタンをクリックすると、
         pageCount = 1;
-        searchword = "";
+        searchNoword = "";
     //初期値１に戻し、
     $(".lists").empty();
     //各項目は何も表示されない
